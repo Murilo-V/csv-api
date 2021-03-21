c47d29cec53b3d0186b7bf652ae6710841f5c4de
Murilo-V <alveiramurilo@hotmail.com>
2021-03-21 20:38:27 -0300

    feat: csv to json

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import csvtojson from 'csvtojson';
 import { PORT } from './config/constants';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', async (req: Request, res: Response) => {
-    res.send({message: 'Hello World'});
+app.get('/', (req: Request, res: Response) => {
+    csvtojson().fromFile('students.csv').then(students => {
+        res.json(students);
+    })
 });
 
 app.listen(PORT);
